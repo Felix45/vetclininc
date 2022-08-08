863f73b48d9d11a5678aee6278edae717db6dd64
Felix Ouma <fatonoh@gmail.com>
2022-08-08 21:39:54 +0300

    Create table animals in vet_clinic

--- schema.sql
+++ schema.sql
@@ -1,3 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals(
+  id INT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255) NOT NULL,
+  date_of_birth date NOT NULL,
+  escape_attempts INT NOT NULL,
+  neutered boolean NOT NULL,
+  weight_kg decimal NOT NULL
+);
