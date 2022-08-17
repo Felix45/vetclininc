c45c9ceb629fbdc85bdc656407e1bf6fb78678a9
Felix Ouma <fatonoh@gmail.com>
2022-08-17 13:34:23 +0300

    Add Join tables and indices to the clinic database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,11 +1,14 @@
+----- Create Clinic Database -----
 CREATE DATABASE clinic;
 
+----- patients table -----
 CREATE TABLE patients(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   date_of_birth DATE NOT NULL
 );
 
+----- medical histories table -----
 CREATE TABLE medical_histories(
   id SERIAL PRIMARY KEY,
   admitted_at TIMESTAMP NOT NULL,
@@ -14,6 +17,7 @@ CREATE TABLE medical_histories(
   CONSTRAINT fk_patient_constraint FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
 
+----- invoices table -----
 CREATE TABLE invoices(
   id SERIAL PRIMARY KEY,
   total_amount DECIMAL(5, 2),
@@ -23,12 +27,14 @@ CREATE TABLE invoices(
   CONSTRAINT fk_invoice_constraint FOREIGN KEY(medical_history_id) REFERENCES invoices(id)
 );
 
+----- treatments table -----
 CREATE TABLE treatments(
   id SERIAL PRIMARY KEY,
   type VARCHAR(255) NOT NULL,
   name VARCHAR(255) NOT NULL
 );
 
+----- invoice items table -----
 CREATE TABLE invoice_items(
   id SERIAL PRIMARY KEY,
   unit_price DECIMAL(5, 2),
@@ -39,3 +45,20 @@ CREATE TABLE invoice_items(
   CONSTRAINT fk_invoice_item_constraint FOREIGN KEY(invoice_id) REFERENCES invoices(id),
   CONSTRAINT fk_treatment_constraint FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
+
+----- medical_histories_treatments join table -----
+CREATE TABLE medical_histories_treatments(
+  id SERIAL PRIMARY KEY,
+  history_id INT NOT NULL,
+  treatment_id INT NOT NULL,
+  CONSTRAINT fk_medical_history_constraint FOREIGN KEY(history_id) REFERENCES medical_histories(id),
+  CONSTRAINT fk_medical_history_treatment_constraint FOREIGN KEY(treatment_id) REFERENCES treatments(id)
+);
+
+
+----- Indexes to fasten SQL Queries ------
+CREATE INDEX idx_medical_history_patients_id ON medical_histories(patient_id);
+CREATE INDEX idx_medical_history_invoices_id ON invoices(medical_history_id);
+
+CREATE INDEX idx_invoice_items_invoices_id ON invoice_items(invoice_id);
+CREATE INDEX idx_invoice_items_treatment_id ON invoice_items(treatment_id);
